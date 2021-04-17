1c34110dfabf893a3d5fd76237588d1eec7e7646
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-04-17 11:25:02 +0200

    Add version file

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,8 @@ module CodeRed
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.autoload_paths << Rails.root.join("lib")
+
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
---
+++ lib/code_red/version.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module CodeRed
+  module Version
+    MAJOR = 0
+    MINOR = 1
+    PATCH = 0
+    PRE   = nil
+
+    VERSION = [MAJOR, MINOR, PATCH].compact.join(".")
+
+    STRING = [VERSION, PRE].compact.join("-")
+  end
+
+  VERSION = CodeRed::Version::STRING
+end
