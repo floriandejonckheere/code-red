ea07bca57b4a49d00af043cb504cb91434a828a7
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-04-24 10:25:46 +0200

    Add Node#inspect

--- app/graph/node.rb
+++ app/graph/node.rb
@@ -4,4 +4,8 @@ class Node
   include ActiveModel::Model
   include ActiveModel::Attributes
   include ActiveModel::Validations
+
+  def inspect
+    "#<#{self.class.name} #{attributes.compact.map { |k, v| [k, v].join('=') }.join(' ')}>"
+  end
 end
