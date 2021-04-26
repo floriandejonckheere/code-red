e32047e9ca195b277cff179e7540d0c244cf8e1f
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-04-26 18:25:39 +0200

    Register TailwindCSS components

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,3 +7,15 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
+
+// Import and register all TailwindCSS Components
+import { Alert, Autosave, Dropdown, Modal, Tabs, Popover, Toggle, Slideover } from "tailwindcss-stimulus-components"
+
+application.register('alert', Alert)
+application.register('autosave', Autosave)
+application.register('dropdown', Dropdown)
+application.register('modal', Modal)
+application.register('tabs', Tabs)
+application.register('popover', Popover)
+application.register('toggle', Toggle)
+application.register('slideover', Slideover)
