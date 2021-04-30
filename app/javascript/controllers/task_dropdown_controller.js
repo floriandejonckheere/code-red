b18c31d0e702eeddf21b2992fc6094f5f573ef70
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-04-30 17:53:23 +0200

    Inherit dropdown controller from stimulus controller

--- app/javascript/controllers/task_dropdown_controller.js
+++ app/javascript/controllers/task_dropdown_controller.js
@@ -1,7 +1,22 @@
-import { Dropdown } from "tailwindcss-stimulus-components";
+import { Controller } from "stimulus";
 
-export default class extends Dropdown {
-  static targets = ['menu', 'button', 'title', 'value']
+export default class extends Controller {
+  static targets = ["menu", "title", "value"]
+
+  static values = {
+    // Dropdown state
+    open: Boolean,
+  }
+
+  toggle() {
+    this.openValue = !this.openValue
+  }
+
+  hide(event) {
+    if (this.element.contains(event.target) === false && this.openValue) {
+      this.openValue = false
+    }
+  }
 
   set(e) {
     // Set hidden form input
@@ -12,5 +27,21 @@ export default class extends Dropdown {
 
     // Dispatch change event, so autosave triggers
     this.valueTarget.dispatchEvent(new Event("change"))
+  }
+
+  openValueChanged() {
+    this.openValue ? this._show() : this._hide()
+  }
+
+  _show() {
+    setTimeout(() => {
+      this.menuTarget.classList.remove("hidden")
+    })
+  }
+
+  _hide() {
+    setTimeout(() => {
+      this.menuTarget.classList.add("hidden")
+    })
   }
 }
