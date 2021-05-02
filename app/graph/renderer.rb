45295127d3e0be7a8e15652d706f9dc9e281a317
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-05-02 20:38:15 +0200

    Add root node

--- app/graph/renderer.rb
+++ app/graph/renderer.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
 class Renderer
-  attr_reader :graph
+  attr_reader :graph, :edge_id
 
   def initialize(graph)
     @graph = graph
+    @edge_id = 0
   end
 
   def to_h
     {
-      nodes: nodes,
-      edges: edges,
+      nodes: [root_node] + nodes,
+      edges: root_edges + edges,
     }
   end
 
@@ -29,19 +30,37 @@ class Renderer
   end
 
   def edges
-    i = 0
-
     graph.tasks.flat_map do |task|
       task.relationships.select { |_k, v| v[:inverse_of].nil? }.keys.flat_map do |relationship_type|
         task.send(relationship_type).map do |node|
           {
-            id: (i += 1),
+            id: (@edge_id += 1),
             source: task.id,
             target: node.id,
             label: relationship_type.to_s.titleize,
           }
         end
       end
+    end
+  end
+
+  def root_node
+    {
+      id: "root",
+      label: graph.name,
+      x: 0,
+      y: 0,
+      size: 3,
+    }
+  end
+
+  def root_edges
+    graph.tasks.select { |t| t.type == "epic" }.map do |task|
+      {
+        id: (@edge_id += 1),
+        source: "root",
+        target: task.id,
+      }
     end
   end
 end
--- spec/graph/renderer_spec.rb
+++ spec/graph/renderer_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Renderer do
 
   let(:graph) { build(:graph) }
 
-  let!(:task0) { create(:task, graph: graph) }
+  let!(:task0) { create(:task, graph: graph, type: "epic") }
   let!(:task1) { create(:task, graph: graph) }
 
   before { create(:edge, graph: graph, from: task0, type: "related_to", to: task1) }
@@ -14,8 +14,9 @@ RSpec.describe Renderer do
     it "returns a list of nodes" do
       expect(renderer.to_h.fetch(:nodes))
         .to match_array [
-          { id: task0.id, label: task0.title, x: a_kind_of(Integer), y: a_kind_of(Integer), size: 3 },
-          { id: task1.id, label: task1.title, x: a_kind_of(Integer), y: a_kind_of(Integer), size: 3 },
+          including(id: "root", label: graph.name, x: 0, y: 0),
+          including(id: task0.id, label: task0.title),
+          including(id: task1.id, label: task1.title),
         ]
     end
   end
@@ -24,7 +25,8 @@ RSpec.describe Renderer do
     it "returns a list of edges" do
       expect(renderer.to_h.fetch(:edges))
         .to match_array [
-          { id: 1, source: task0.id, target: task1.id, label: "Related To" },
+          including(id: 1, source: "root", target: task0.id),
+          including(id: 2, source: task0.id, target: task1.id, label: "Related To"),
         ]
     end
   end
