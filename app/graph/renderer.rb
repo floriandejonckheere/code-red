07145c501f4294f2978c5e8b93ff39d2f8ad0db1
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-05-08 14:47:49 +0200

    Do not render root node

--- app/graph/renderer.rb
+++ app/graph/renderer.rb
@@ -11,8 +11,8 @@ class Renderer
 
   def to_h
     {
-      nodes: [root_node] + nodes,
-      edges: root_edges + edges,
+      nodes: nodes,
+      edges: edges,
     }
   end
 
@@ -35,29 +35,12 @@ class Renderer
       task.relationships.select { |_k, v| v[:inverse_of].nil? }.keys.flat_map do |relationship_type|
         task.send(relationship_type).map do |node|
           {
-            source: nodes.find_index { |n| n[:id] == task.id } + 1,
-            target: nodes.find_index { |n| n[:id] == node.id } + 1,
+            source: nodes.find_index { |n| n[:id] == task.id },
+            target: nodes.find_index { |n| n[:id] == node.id },
             label: relationship_type.to_s.titleize,
           }
         end
       end
-    end
-  end
-
-  def root_node
-    {
-      id: "root",
-      label: graph.name,
-      icon: nil,
-    }
-  end
-
-  def root_edges
-    graph.tasks.select { |t| t.type == "epic" }.map do |task|
-      {
-        source: 0,
-        target: nodes.find_index { |n| n[:id] == task.id } + 1,
-      }
     end
   end
 
