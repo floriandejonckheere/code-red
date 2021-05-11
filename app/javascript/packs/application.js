f8f7a927421e0f6eaa240aa528ae9e3a6d40d9c7
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-05-11 16:29:25 +0200

    Add logo and favicon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@ window.Rails = require('@rails/ujs')
 import 'controllers'
 
 import './application.scss'
-import '../images/splash.jpg'
+import '../images/logo.png'
 
 import * as cola from "webcola/dist"
 
