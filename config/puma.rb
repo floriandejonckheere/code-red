59f4f496ca0f9d391de06e5a7937f3636e062dc1
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-05-13 16:06:14 +0200

    Enable configurable concurrency

--- config/puma.rb
+++ config/puma.rb
@@ -31,8 +31,8 @@ pidfile ENV.fetch("PIDFILE", "tmp/pids/server.pid")
 # the concurrency of the application would be max `threads` * `workers`.
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
-#
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+
+workers ENV.fetch("WEB_CONCURRENCY", 2)
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
